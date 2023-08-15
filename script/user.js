3039d8aa8444d28cbfb54f4eb9aff1a3406c2136
Mirzarahmon <rahmonraxmatov@gmail.com>
2023-08-15 22:58:42 +0500

    first push

---
+++ script/user.js
@@ -0,0 +1,37 @@
+const API = "https://api.escuelajs.co/api/v1/products";
+const ID = +window.location.hash.replace("#", "");
+const userBlock = document.querySelector("#user__block");
+const avatar = document.querySelector("#images");
+const title = document.querySelector("#title");
+const price = document.querySelector("#price");
+const description = document.querySelector("#description");
+const userBtn = document.querySelector("#user__btn");
+const countElement = document.querySelector("#count");
+async function getInfo() {
+  try {
+    const response = await fetch(`${API}/${ID}`);
+    const data = await response.json();
+    if (data) {
+      avatar.src = data.images;
+      title.innerHTML = data.title;
+      price.innerHTML = data.price;
+      description.innerHTML = data.description;
+      updatedAt.innerHTML = data.updatedAt;
+      console.log(data);
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+getInfo();
+
+let count = localStorage.getItem("count") || 0;
+countElement.textContent = count;
+userBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  count++;
+  countElement.textContent = count;
+  localStorage.setItem(`count`, count);
+  console.log(count);
+});
